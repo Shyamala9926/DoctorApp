1afa6e3f9c4a291879edc132446a31ce0939c1b5
Shyamala9926 <shyamlajanamoni2002@gmail.com>
2025-02-18 18:37:40 +0530

    new changes in code

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10' >
       {/*----- left side-----*/}
-      <div>
+      <div className='flex-1 py-8 sm:py-10 md:py-16 lg:py-24 lg:pl-5'>
         <div>
           <p> Book Appointment</p>
           <p>With 100+ Trusted Docters</p>
