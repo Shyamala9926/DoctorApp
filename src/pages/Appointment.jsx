9e23e5b30f677da1a79d0039b9e08a8869a6d966
Shyamala9926 <shyamlajanamoni2002@gmail.com>
2025-03-21 22:15:01 +0530

    adding new doctor appointment page

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 function Appointment() {
-  return (
-    <div>Appointment</div>
+
+  const  {docID} = useParams()
+  const {doctors}= useContext(AppContext) 
+
+  const [docInfo, setDocInfo]= userState(null)
+
+  const fetchDocInfo = async()=>{
+    const docInfo = doctors.find(doc=> doc._id === docId)
+    setDocInfo(docInfo)
+    console.log(docInfo)
+  }
+
+  useEffect(()=>{
+    fetchDocInfo()
+  },[doctors,docID])
+  return docInfo && (
+    <div>
+      {/*----- Doctor Details -----*/}
+      <div>
+        <div>
+          <img src={docInfo.image} alt=''/>
+        </div>
+      </div>
+    </div>
   )
 }
 
