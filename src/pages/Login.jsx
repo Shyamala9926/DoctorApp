aba5473b3d5a0d2d66ae5dedd175c5b85e09a617
Shyamala9926 <shyamlajanamoni2002@gmail.com>
2025-05-16 15:15:29 +0530

    conflicts

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,13 +16,15 @@ function Login() {
     <form className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px]sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
         <p className='text-2xl font-semibold'>{state=== 'Sign Up' ? "Create Account":"Login"}</p>
-        <p>please { state ==='Sign Up'? "sign up": "login"}</p>
-
-        <div className='w-full'>
+        <p>please { state ==='Sign Up'? "sign up": "login"} to book appointment</p>
+{
+  state=== "Sign Up"&& <div className='w-full'>
           <p>Full Name</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='text' onChange={(e)=>setName(e.target.name)} value={name} required/>        
           </div>
 
+}
+       
           <div className='w-full'>
           <p>Email</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='email' onChange={(e)=>setEmail(e.target.name)} value={name} required />        
@@ -36,8 +38,8 @@ function Login() {
 <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state=== 'Sign Up' ? "Create Account":"Login"}</button>
       {
         state === "Sign Up"
-        ? <p>Already have an account?Login here</p>
-        : <p>Create an new account?click here</p>
+        ? <p>Already have an account?<span onClick={()=>setState('Login')} className="text-primary underline cursor-pointer">Login here</span></p>
+        : <p>Create an new account?<span onClick={()=>setState('Sign Up')} className="text-primary underline cursor-pointer">click here</span></p>
       }</div>
 
 
